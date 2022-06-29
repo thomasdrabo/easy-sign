4de58f361837f4c9a5dece06aee0bf5b608cf7a4
watchsz <96117061+watchsz@users.noreply.github.com>
2022-06-29 16:18:19 +0200

    ajout import de fichier exclusivement xml

--- EasySign/lib/pages/page2.dart
+++ EasySign/lib/pages/page2.dart
@@ -1,7 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:easy_sign/widgets/buttons/ui_gradient_button.dart';
+import 'package:file_picker/file_picker.dart';
 
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
   const Page2({Key? key}) : super(key: key);
+
+  @override
+  State<Page2> createState() => _Page2State();
+}
+
+final FirebaseAuth auth = FirebaseAuth.instance;
+final firestoreInstance = FirebaseFirestore.instance;
+class _Page2State extends State<Page2> {
+  PlatformFile? pickedFile;
+
+  Future _selectFile() async {
+    final result = await FilePicker.platform.pickFiles(type: FileType.custom, allowedExtensions: ['xml']);
+    if (result == null) return;
+
+    setState(() {
+      pickedFile = result.files.first;
+    });
+  }
+
+  Future _uploadFile() async {
+    final path = 'users/' + auth.currentUser!.uid + '/testXML'; // Changer pour l'upload de fichier
+    dynamic file = File(pickedFile!.path!);
+
+    Reference firebaseStorageRef = FirebaseStorage.instance.ref().child(path);
+    UploadTask uploadTask = firebaseStorageRef.putFile(file);
+    TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+    taskSnapshot.ref.getDownloadURL().then((value) {
+      firestoreInstance.collection('users').doc(auth.currentUser!.uid).update({'txml': value});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +44,31 @@ class Page2 extends StatelessWidget {
       backgroundColor: Colors.blue,
       body: SafeArea(
         child: Container(
-          child: Text('PAGE 2'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              UI_gradient_button(title: 'Sign', onPressed: () {
+                
+              })
+              // UI_gradient_button(title: 'Import Document', onPressed: () {
+              //   _selectFile();
+              // }),
+              // Text(
+              //   (pickedFile!.path!),
+              //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              // ),
+              // const SizedBox(height:10), 
+              // UI_gradient_button(title: 'Send Document', onPressed: () {
+              //   _uploadFile();
+              // }),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+class File {
+  var myFile = File('file.txt');
+  File(Object? s);
 }
